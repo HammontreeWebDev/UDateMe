a060cc28e2df9b0f269e097db470bf221075de55
HammontreeWebDev <hammontreewebdevelopment@gmail.com>
2023-04-10 16:26:35 -0400

    basic form added for sign up component

--- client/src/components/SignUp/index.jsx
+++ client/src/components/SignUp/index.jsx
@@ -1,10 +1,42 @@
 import React from "react";
 
+import { useMutation } from '@apollo/client';
+import { ADD_USER } from '../../utils/mutations';
+
+import Auth from '../../utils/auth';
+
+// BootStrap:
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+
 const SignUp = () => {
-    return(
-        <p>
-            Dis is da sign up!
-        </p>
+    return (
+        <Form>
+
+            <Form.Group className="mb-3" controlId="formBasicUsername">
+                <Form.Label>Username</Form.Label>
+                <Form.Control type="username" placeholder="Enter username" />
+                <Form.Text className="text-muted">
+                    This is the name we will use to greet you.
+                </Form.Text>
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control type="email" placeholder="Enter email" />
+                <Form.Text className="text-muted">
+                    We'll never share your email with anyone else.
+                </Form.Text>
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+                Submit
+            </Button>
+        </Form>
     )
 }
 
