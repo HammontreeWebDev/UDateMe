4a04155e563fd8d77d6effe00ba73e816dd6027d
HammontreeWebDev <hammontreewebdevelopment@gmail.com>
2023-04-10 12:19:17 -0400

    set up server.js file to use graphql / Apollo

--- server/server.js
+++ server/server.js
@@ -1,78 +1,43 @@
-// const express = require("express");
-// const { ApolloServer } = require("apollo-server-express");
-// const path = require("path");
-// const { authMiddleware } = require("./utils/auth");
+const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
+const path = require('path');
+const { authMiddleware } = require('./utils/auth');
 
-// // Web Socket:
-// const { createServer } = require('http');
-// const { ApolloServerPluginDrainHttpServer } = require('@apollo/server/plugin/drainHttpServer');
-// const { makeExecutableSchema } = require('@graphql-tools/schema');
-// const { WebSocketServer } = require('ws');
-// const { useServer } = require('graphql-ws/lib/use/ws');
+const { typeDefs, resolvers } = require('./schemas');
+const db = require('./config/connection');
 
-// const { typeDefs, resolvers} = require("./schemas");
-// const db = require("./config/connection");
+const PORT = process.env.PORT || 3001;
+const app = express();
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware,
+});
 
-// const PORT = process.env.PORT || 3001;
-// const app = express();
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
-// // Web socket:
-// const httpServer = createServer(app);
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
 
-// const schema = makeExecutableSchema({ typeDefs, resolvers });
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
-// const server = new ApolloServer({
-//   schema,
-//   plugins: [
-//     ApolloServerPluginDrainHttpServer({ httpServer }),
 
-//     {
-//       async serverWillStart() {
-//         return {
-//           async drainServer() {
-//             await serverCleanup.dispose();
-//           }
-//         }
-//       }
-//     }
-//   ],
-//   context: authMiddleware,
-//   cache: "bounded",
-// });
-
-// // Creating the WebSocket server
-// const wsServer = new WebSocketServer({
-//   // This is the `httpServer` we created in a previous step.
-//   server: httpServer,
-//   path: '/graphql',
-// });
-
-// // start listening.
-// const serverCleanup = useServer({ schema }, wsServer);
-
-// app.use(express.urlencoded({ extended: false }));
-// app.use(express.json());
-
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static(path.join(__dirname, "../client/build")));
-// }
-
-// app.get("/*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "../client/build/index.html"));
-// });
-
-// const startApolloServer = async (schema) => {
-//   await server.start();
-//   server.applyMiddleware({ app });
-
-//   db.once("open", () => {
-//     httpServer.listen(PORT, () => {
-//       console.log(`API Server is running on port ${PORT}!`);
-//       console.log(
-//         `Navigate to GraphQL at http://localhost:${PORT}${server.graphqlPath}`
-//       );
-//     });
-//   });
-// };
-
-// startApolloServer(typeDefs, resolvers);
+// Create a new instance of an Apollo server with the GraphQL schema
+const startApolloServer = async (typeDefs, resolvers) => {
+  await server.start();
+  server.applyMiddleware({ app });
+  
+  db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+    })
+  })
+  };
+  
+// start the server
+  startApolloServer(typeDefs, resolvers);
