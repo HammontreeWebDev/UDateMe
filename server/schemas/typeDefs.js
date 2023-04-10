e2476f1986dc49515d4390f87351b317adb83dc3
HammontreeWebDev <hammontreewebdevelopment@gmail.com>
2023-04-10 12:50:33 -0400

    type defs updated for this specific projects (copied structures from existing project of mine)

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,62 +1,29 @@
-// const { gql } = require("apollo-server-express");
+const { gql } = require("apollo-server-express");
 
-// const typeDefs = gql`
-//   type User {
-//     _id: ID
-//     username: String
-//     email: String
-//     groups: [Group]
-//   }
+const typeDefs = gql`
+  type User {
+    _id: ID
+    username: String
+    email: String
+  }
 
-//   type Message {
-//     body: String
-//     user: User
-//     timestamp: Date
-//     updated: Boolean
-//   }
+  scalar Date
 
-//   scalar Date
+  type Auth {
+    token: ID!
+    user: User
+  }
 
-//   type Group {
-//     _id: ID
-//     name: String
-//     members: [User]
-//     admins: [User]
-//     messages: [Message]
-//   }
+  type Query {
+    user(userId: ID!): User
+    userByName(username: String!): User
+  }
 
-//   type Auth {
-//     token: ID!
-//     user: User
-//   }
-
-//   type Query {
-//     user(userId: ID!): User
-//     group(groupId: ID!): Group
-//     userByName(username: String!): User
-//   }
-
-//   type Mutation {
-//     login(email: String!, password: String!): Auth
-//     addUser(username: String!, email: String!, password: String!): Auth
-//     createGroup(name: String!, admin: ID!): Group
-//     updateGroup(groupId: ID!, name: String!, admin: ID!): Group
-//     deleteGroup(groupId: ID!, admin: ID!): Group
-//     addGroupMember(userId: ID!, groupId: ID!, admin: ID!): Group
-//     removeGroupMember(userId: ID!, groupId: ID!, admin: ID!): Boolean
-//     postMessage(body: String!, groupId: ID!, username: String!):Group
-//     leaveGroup(userId: ID!, groupId: ID!):Group
-//   }
-
-//   type Subscription {
-//     messageAdded: MessageSubscriptionPayload!
-//   }
-
-//   type MessageSubscriptionPayload {
-//     mutation: String!
-//     data: Group!
-//   }
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+  }
   
-// `;
+`;
 
-// module.exports = typeDefs;
+module.exports = typeDefs;
