48156917a2595a0724bb1a9c4671a5d5b0d3c691
HammontreeWebDev <hammontreewebdevelopment@gmail.com>
2023-04-10 14:55:50 -0400

    landing page navbar component added to landing page

---
+++ client/src/components/LandingNavBar/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const LandingNavBar = () => {
+
+    return(
+        <p>
+            This is the NavBar Component
+        </p>
+    )
+};
+
+export default LandingNavBar;
--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,16 +1,22 @@
 import React, { useState } from 'react';
 import "./assets/css/landing.css";
 
+// components:
+import LandingNavBar from '../components/LandingNavBar';
+
 // Landing Page
 
 const Landing = () => {
 
     return (
+        <>
+        <LandingNavBar />
         <p>
             Hello World!
             <br></br>
             This is the landing page!
         </p>
+        </>
     );
 }
 
