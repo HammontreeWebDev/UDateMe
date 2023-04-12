48949b5c59e046f2c3d0eac8680d6e54c5b3509b
HammontreeWebDev <hammontreewebdevelopment@gmail.com>
2023-04-12 16:49:19 -0400

    added text to FAQ section

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -50,25 +50,29 @@ const Landing = () => {
                 <Accordion.Item eventKey="0">
                     <Accordion.Header className='accordion-header'>Getting Started</Accordion.Header>
                     <Accordion.Body>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-                        minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                        aliquip ex ea commodo consequat. Duis aute irure dolor in
-                        reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-                        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-                        culpa qui officia deserunt mollit anim id est laborum.
+                        You've taken the first step already by visiting this application!
+                        <br></br>
+                        <br></br>
+                        In order to fully unlock everything that this application has to
+                        offer you, simply click on the hamburger icon in the nav bar and sign up for a new account.
+                        <br></br>
+                        <br></br>
+                        Don't worry, it's free!
                     </Accordion.Body>
                 </Accordion.Item>
                 <Accordion.Item eventKey="1">
                     <Accordion.Header className='accordion-header'>Is this a dating app?</Accordion.Header>
                     <Accordion.Body>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-                        minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                        aliquip ex ea commodo consequat. Duis aute irure dolor in
-                        reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-                        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-                        culpa qui officia deserunt mollit anim id est laborum.
+                        This is not a typical 'dating app'. We don't pair you with other people or allow you to view other people's profiles that they have created.
+                        <br></br>
+                        <br></br>
+                        Instead, we aim to encourage you in your newly found relationship or even a relationship that you have been in for a long time. We offer fun things like random conversation starters to use between you and your partner. Feel free to make a game out of it!
+                        <br></br>
+                        <br></br>
+                        We also offer things like restaraunt and entertainment suggestions that are based off your location.
+                        <br></br>
+                        <br></br>
+                        You can also create a 'partner profile' where you are given the opportunity to take a quiz on your partner's behalf (or you can have them take the quiz). Based off these answers, we will create a custom generated list of gift ideas for you to be able to use for any given occasion.
                     </Accordion.Body>
                 </Accordion.Item>
             </Accordion>
