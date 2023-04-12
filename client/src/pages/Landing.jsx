c9c47f6aeb129136dea84cfa4e738ed21e10acd3
HammontreeWebDev <hammontreewebdevelopment@gmail.com>
2023-04-12 15:55:04 -0400

    added some text shadow, etc

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -1,4 +1,9 @@
 import React, { useState } from 'react';
+
+// Bootstrap:
+import Accordion from 'react-bootstrap/Accordion';
+
+// CSS:
 import "./assets/css/landing.css";
 
 // components:
@@ -37,11 +42,37 @@ const Landing = () => {
 
     const MainContent = () => {
         return (
-            <p>
-                Hello World!
-                <br></br>
-                This is the landing page!
-            </p>
+            <>
+            <h1 className='landing-header'>
+                Welcome to UDateMe!
+            </h1>
+            <Accordion defaultActiveKey="0" className='custom-accordion'>
+                <Accordion.Item eventKey="0">
+                    <Accordion.Header className='accordion-header'>Getting Started</Accordion.Header>
+                    <Accordion.Body>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                        minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                        aliquip ex ea commodo consequat. Duis aute irure dolor in
+                        reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                        culpa qui officia deserunt mollit anim id est laborum.
+                    </Accordion.Body>
+                </Accordion.Item>
+                <Accordion.Item eventKey="1">
+                    <Accordion.Header className='accordion-header'>Is this a dating app?</Accordion.Header>
+                    <Accordion.Body>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                        minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                        aliquip ex ea commodo consequat. Duis aute irure dolor in
+                        reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                        culpa qui officia deserunt mollit anim id est laborum.
+                    </Accordion.Body>
+                </Accordion.Item>
+            </Accordion>
+            </>
         )
     };
 
