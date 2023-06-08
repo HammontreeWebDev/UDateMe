86bee470043a5aa665ab15baf3fef1c6bc905238
HammontreeWebDev <hammontreewebdevelopment@gmail.com>
2023-06-07 21:49:42 -0400

    question list working

--- client/src/components/ConversationStarters/index.jsx
+++ client/src/components/ConversationStarters/index.jsx
@@ -1,11 +1,25 @@
 import React from 'react';
+import questionList from '../../utils/conversation';
 
 const ConversationStarters = () => {
 
-    return(
+    // function to choose random question from Question List
+    function fetchConversationStarter() {
+        const generatedResponseEl = document.querySelector('#generated-response');
+        const randomQuestion = Math.floor(Math.random() * questionList.length);
+        const chosenQuestion = questionList[randomQuestion];
+        generatedResponseEl.textContent = chosenQuestion;
+    }
+    
+
+    return (
+        <>
         <p>
             This is the Conversation Starters Component!
+            <button onClick={fetchConversationStarter}>Convo Starter</button>
         </p>
+        <p id='generated-response'></p>
+        </>
     )
 };
 
---
+++ client/src/utils/conversation.js
@@ -0,0 +1,9 @@
+const questionList = [
+    'What kind of pizza do you like?',
+    'What is your name?',
+    'Doggy Style?',
+    'Men or Women?',
+    'Do you like me?'
+]
+
+export default questionList;
